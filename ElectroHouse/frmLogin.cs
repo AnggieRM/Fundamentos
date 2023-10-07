f6684f306b531f40a3207deead4debb161f2957d
Carlos Martinez <u202010153@upc.edu.pe>
2023-10-07 15:49:58 -0500

    login terminado
    modifciado

--- ElectroHouse/frmLogin.cs
+++ ElectroHouse/frmLogin.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -30,36 +31,67 @@ namespace ElectroHouse
 
         private void button5_Click(object sender, EventArgs e)
         {
+            string usuario = txtUsuario.Text;
+            string contrasenia = txtContraseña.Text;
 
-            //Form1 obj = new Form1();
-            //obj.Show();
-            //this.Hide();
-
-            //obj.FormClosing += frm_closing;
-
-            Login login = new Login();
-            login.usuario = txtUsuario.Text;
-            login.contrasenia = txtContraseña.Text;
-
-            int result = Registro.AgregarUsuario(login);
-            if (result == 0)
+            // Realizar la verificación de las credenciales
+            if (VerificarCredenciales(usuario, contrasenia))
             {
-                MessageBox.Show("Error al guardar");
+                MessageBox.Show("Inicio de sesión exitoso");
+                // Aquí puedes redirigir al usuario a la página principal o realizar otras acciones
+                Form1 form1 = new Form1();
+
+                // Mostrar Form2 utilizando el método Show
+                form1.Show();
+
+                // Opcionalmente, ocultar el formulario actual (Form1) si deseas
+                this.Hide();
+
             }
             else
             {
-                MessageBox.Show("Exito al guardar");
+                MessageBox.Show("Credenciales incorrectas");
             }
         }
 
-        //private void frm_closing(object sender, FormClosingEventArgs e)
-        //{
-        //    this.Show();
-
-        //}
-        private void label1_Click(object sender, EventArgs e)
+        private bool VerificarCredenciales(string usuario, string contrasenia)
         {
+            string connectionString = "";
+            SqlConnection connection = new SqlConnection("Integrated Security=SSPI;Persist Security Info=False;User ID=User001;Initial Catalog=dbtest;Data Source=LAPTOP-31D55N99");
 
+            try
+            {
+                connection.Open();
+                string query = "SELECT COUNT(*) FROM datos WHERE usuario = @Usuario AND contrasenia = @Contrasenia";
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@Usuario", usuario);
+                    command.Parameters.AddWithValue("@Contrasenia", contrasenia);
+
+                    int count = (int)command.ExecuteScalar();
+                    return count > 0;
+                }
+            }
+            catch (Exception ex)
+            {
+                // Maneja cualquier excepción que pueda ocurrir durante la verificación
+                MessageBox.Show("Error: " + ex.Message);
+                return false;
+            }
+            finally
+            {
+                connection.Close();
+            }
+            //private void frm_closing(object sender, FormClosingEventArgs e)
+            //{
+            //    this.Show();
+
+            //}
         }
+           private void label1_Click(object sender, EventArgs e)
+            {
+
+            }
+        
     }
 }
